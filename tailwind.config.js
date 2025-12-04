dbaca5d978d74cf99b8e9c7ec77048a4b10c84b7
ARR2005 <Aldwinrich90@gmail.com>
2025-12-04 22:37:48 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        tan: {
+          100: '#e6dccf',
+        },
+        brown:{
+            900: '#3e2723'
+        }
+      }
+    },
+  },
+}
